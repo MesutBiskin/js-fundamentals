36d40506ad0e40e4077dcd0a1f8a550bb3c7feef
Mesut Biskin <msbiskin@gmail.com>
2023-10-04 16:31:35 -0400

    Js test

---
+++ test.js
@@ -0,0 +1,15 @@
+// Blah Blah Blah 
+
+const age = 48
+const firstName = 'Mesut'
+
+console.log("Hello.")
+
+console.log("My name is " + firstName)
+
+// String variable string.
+console.log("I am "  + age + " " + "years old.")
+// console.log("I am",age )
+
+
+
